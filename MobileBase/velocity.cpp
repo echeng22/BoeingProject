30f932d2932516e25cefe3f86cbfe695304a4fcf
yuchenrao <yuchenrao2017@u.northwestern.edu>
2017-05-12 16:51:37 -0500

    base velocity to angular velocity

---
+++ MobileBase/velocity.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "velocity.h"
+using namespace std;
+
+void calVelocity(int vx, int vy, int wz, double *u){
+    int i;
+    double A[4][3] = {{-LENGTH-WIDTH, 1, -1}, {LENGTH+WIDTH, 1, 1}, {LENGTH+WIDTH, 1, -1}, {-LENGTH-WIDTH, 1, 1}};
+    double V[3]= {wz, vx, vy};
+    // matrix calculation
+    for (i = 0; i < 4; i++){
+        u[i] = (A[i][0] * V[0] + A[i][1] * V[1] + A[i][2] *V[2])/RADIUS;
+    }
+}
+
+int main()
+{
+    double a[4];
+    calVelocity(0., 0., 1., a);
+    cout << a[0]<<endl;
+    cout << a[1]<<endl;
+    cout << a[2]<<endl;
+    cout << a[3]<<endl;
+    return 0;
+}
---
+++ MobileBase/velocity.h
@@ -0,0 +1,14 @@
+// This is start of the header guard.  ADD_H can be any unique name.  By convention, we use the name of the header file.
+#ifndef ADD_H
+#define ADD_H
+
+#define LENGTH 0.2
+#define RADIUS 0.1
+#define WIDTH  0.2
+#define HEIGHT 0.1
+
+// This is the content of the .h file, which is where the declarations go
+void calVelocity(int vx, int vy, int wz, double *u);
+
+// This is the end of the header guard
+#endif
