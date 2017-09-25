16f80a0aeebe7e3a83d5c93685a55a12fe66fff8
Yuchen Rao <yuchenrao2017@u.northwestern.edu>
2017-09-25 16:22:47 -0500

    change track.py

--- MobileBase/track.py
+++ MobileBase/track.py
@@ -10,30 +10,32 @@ class trackControl(object):
     def __init__(self):
 
         self._trackPub = rospy.Publisher("vel", Twist, queue_size = 1)
-        self.Rate = rospy.Rate(10)
+        self.Rate = rospy.Rate(100)
         self.twist_mag = Twist()
-        self.T = 10
-        self.pos_x = 0
+        self.T = 0.01
+        self.r = 1
+        self.pos_x = self.r
         self.pos_y = 0
-        self.num = 50
-        self.r = 10
+        self.num = 5000
         self.trajectory()
 
     def trajectory(self):
 
-        # Circle, clockwise
+        # Circle, counterclockwise
         for t in range(1, self.num + 1):
             theta = t * 2 * 3.1415926 / self.num
-            yp = self.r * math.sin(theta) - self.r
+            yp = self.r * math.sin(theta)
             xp = self.r * math.cos(theta)
             self.control(xp, yp)
             self.pos_x = xp
             self.pos_y = yp
 
+        self.control(xp, yp)
+
     def control(self, x, y):
         # desired point (x, y)
 
-        # Initial Twist massage
+        # Initial Twist message
         self.twist_mag.linear.x = 0
         self.twist_mag.linear.y = 0
         self.twist_mag.angular.z = 0
@@ -41,16 +43,8 @@ class trackControl(object):
         # Calculate the velocity
         det_x = x - self.pos_x
         det_y = y - self.pos_y
-        # forward or backward
-        if det_x == 0:
-            self.twist_mag.linear.y = det_y / self.T
-        # Turn left or right
-        elif det_y == 0:
-            self.twist_mag.linear.x = det_x / self.T
-        # Go slash
-        else:
-            self.twist_mag.linear.x = det_x / self.T
-            self.twist_mag.linear.y = det_y / self.T
+        self.twist_mag.linear.x = det_x / self.T
+        self.twist_mag.linear.y = det_y / self.T
 
         self._trackPub.publish(self.twist_mag)
         self.Rate.sleep()
