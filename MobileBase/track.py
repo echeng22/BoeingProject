3f6e62ff4cd0fcbf58fa73bb8a927f1c8bf44272
Chainatee Tanakulrungson <c.tanakul@gmail.com>
2017-09-27 15:09:34 -0500

    add tracks in track.py, still needs more edit

--- MobileBase/track.py
+++ MobileBase/track.py
@@ -10,9 +10,10 @@ class trackControl(object):
     def __init__(self):
 
         self._trackPub = rospy.Publisher("vel", Twist, queue_size = 1)
-        self.Rate = rospy.Rate(100)
+        self.hz = 100
+        self.Rate = rospy.Rate(self.hz)
         self.twist_mag = Twist()
-        self.T = 0.01
+        self.T = 1.0 / self.hz
         self.r = 1
         self.pos_x = self.r
         self.pos_y = 0
@@ -23,10 +24,48 @@ class trackControl(object):
 
         # Circle, counterclockwise
         for t in range(1, self.num + 1):
-            theta = t * 2 * 3.1415926 / self.num
+            theta = t * 2 * np.pi / self.num
             yp = self.r * math.sin(theta)
             xp = self.r * math.cos(theta)
             self.control(xp, yp)
+            self.pos_x = xp
+            self.pos_y = yp
+
+        # Square, counterclockwise, the radius is half of each side (self.num must be multiplicable by 4), assume self.pos_y = 0 and self.pos_x = self.r
+        for t in range(1, (self.num / 4) + 1) # Straight Up
+            yp = self.pos_y + t * (self.r * 2) / (self.num / 4)
+            self.control(self.pos_x, yp)
+            self.pos_y = yp
+        for t in range(1, (self.num / 4) + 1) # Straight Left
+            xp = self.pos_x - t * (self.r * 2) / (self.num / 4)
+            self.control(xp, self.pos_y)
+            self.pos_x = xp
+        for t in range(1, (self.num / 4) + 1) # Straight Down
+            yp = self.pos_y - t * (self.r * 2) / (self.num / 4)
+            self.control(self.pos_x, yp)
+            self.pos_y = yp
+        for t in range(1, (self.num / 4) + 1) # Straight Right
+            xp = self.pos_x + t * (self.r * 2) / (self.num / 4)
+            self.control(xp, self.pos_y)
+            self.pos_x = xp
+
+        # Triangle, counterclockwise, the radius is half of the side (self.num must be multiplicable by 3), assume self.pos_y = 0 and self.pos_x = self.r
+        for t in range(1, (self.num / 3) + 1) # Bottom Right to Top vortex
+            yp = self.pos_y + t * (self.r * 2) / (self.num / 3) * np.sin(np.pi / 3)
+            xp = self.pos_x - t * (self.r * 2) / (self.num / 3) * np.cos(np.pi / 3)
+            self.control(xp, yp)  
+            self.pos_x = xp
+            self.pos_y = yp
+        for t in range(1, (self.num / 3) + 1) # Top vortex to Bottom Right 
+            yp = self.pos_y - t * (self.r * 2) / (self.num / 3) * np.cos(np.pi / 6)
+            xp = self.pos_x - t * (self.r * 2) / (self.num / 3) * np.sin(np.pi / 6)
+            self.control(xp, yp)  
+            self.pos_x = xp
+            self.pos_y = yp
+        for t in range(1, (self.num / 3) + 1) # Bottom Left to Bottom Right
+            yp = self.pos_y
+            xp = self.pos_x + t * (self.r * 2) / (self.num / 3)
+            self.control(xp, yp)  
             self.pos_x = xp
             self.pos_y = yp
 
